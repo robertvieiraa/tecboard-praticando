6255f2c40d46060a65ec61f85be919a2a605d869
Robert Vieira <robertvieirasa1987@gmail.com>
2026-08-12 17:01:04 -0300

    Criei card de evento

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import './App.css'
 import { FormularioDeEvento } from './components/FormularioDeEvento'
 import { Tema } from './components/Tema'
 import { Banner } from './components/Banner'
+import { CardEvento } from './components/CardEvento'
 
 function App() {
   const temas = [
@@ -35,6 +36,15 @@ function App() {
     }
   ]
 
+  const eventos = [
+    {
+      capa: "https://raw.githubusercontent.com/viniciosneves/tecboard-assets/refs/heads/main/imagem_1.png",
+      tema: temas[0],
+      data: new Date(),
+      titulo: "Mulheres no Front"
+    }
+  ]
+
   return (
     <main>
       <header>
@@ -46,6 +56,7 @@ function App() {
         return (
           <section key={item.id}>
             <Tema tema={item} />
+            <CardEvento evento={eventos[0]}/>
           </section>
         )
       })}
