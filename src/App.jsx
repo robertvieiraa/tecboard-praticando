c969388959e54904a06851543b4741869bca8f9f
Robert Vieira <robertvieirasa1987@gmail.com>
2026-08-12 15:18:51 -0300

    Adicionei lista de temas e respectivas sessões

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,62 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import { FormularioDeEvento } from './components/FormularioDeEvento'
+import { Tema } from './components/Tema'
+import { Banner } from './components/Banner'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const temas = [
+    {
+      id: 1,
+      nome: 'front-end'
+    },
+    {
+      id: 2,
+      nome: 'back-end'
+    },
+    {
+      id: 3,
+      nome: 'devops'
+    },
+    {
+      id: 4,
+      nome: 'inteligência artificial'
+    },
+    {
+      id: 5,
+      nome: 'data science'
+    },
+    {
+      id: 6,
+      nome: 'cloud'
+    }
+  ]
 
   return (
     <main>
       <header>
         <img src="logo.png" />
       </header>
-      <section>
-        <img src="banner.png" />
-      </section>
+      <Banner />
       <FormularioDeEvento />
+      <section>
+        <Tema tema={temas[0]}/>
+      </section>
+      <section>
+        <Tema tema={temas[1]}/>
+      </section>
+      <section>
+        <Tema tema={temas[2]}/>
+      </section>
+      <section>
+        <Tema tema={temas[3]}/>
+      </section>
+      <section>
+        <Tema tema={temas[4]}/>
+      </section>
+      <section>
+        <Tema tema={temas[5]}/>
+      </section>
     </main>
   )
 }
